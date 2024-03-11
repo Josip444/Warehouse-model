1bb540ee2eea473e91ec3ccfcd4d8efe509174ef
Josip444 <simic.josip9@gmail.com>
2024-03-11 20:28:37 +0100

    update button in categories list

--- src/main/java/com/example/skladiste/Controller/CategoryController.java
+++ src/main/java/com/example/skladiste/Controller/CategoryController.java
@@ -3,14 +3,17 @@ package com.example.skladiste.Controller;
 import com.example.skladiste.Model.Category;
 import com.example.skladiste.Repository.CategoryRepository;
 import com.example.skladiste.Service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CategoryController {
@@ -49,6 +52,33 @@ public class CategoryController {
 
         this.categoryService.deleteCarCategory(id);
 
+        return "redirect:/showCategories";
+    }
+
+    @PostMapping ("/updateCarCategory/{id}")
+    public String updateCarCategoryForm(Model model,@PathVariable Long id)
+    {
+        Optional<Category>optionalCategory=this.categoryRepository.findById(id);
+        if(optionalCategory.isPresent())
+        {
+            Category category = optionalCategory.get();
+            model.addAttribute("category",category);
+            return "Category/updateCarCategory";
+        }
+
+        return "redirect:/showCategories";
+    }
+
+    @PostMapping("/updateCarCategory")
+    public String updateCarCategory(@Valid Category category, BindingResult result,Model model)
+
+    {
+
+        if(result.hasErrors()){
+            model.addAttribute("category",category);
+            return "Category/updateCarCategory";
+        }
+        this.categoryService.save(category);
         return "redirect:/showCategories";
     }
 
--- src/main/java/com/example/skladiste/Service/CategoryService.java
+++ src/main/java/com/example/skladiste/Service/CategoryService.java
@@ -1,5 +1,6 @@
 package com.example.skladiste.Service;
 
+import com.example.skladiste.Model.Category;
 import com.example.skladiste.Repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,10 @@ public class CategoryService {
     public void deleteCarCategory(Long id)
     {
         this.categoryRepository.deleteById(id);
+    }
+
+    public void save(Category category)
+    {
+        this.categoryRepository.save(category);
     }
 }
